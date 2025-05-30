c27b87d3e960b12eca3395c443b41301e29e1067
XXXXRT666 <157766680+XXXXRT666@users.noreply.github.com>
2025-05-31 00:50:46 +0800

    Ignore User-Site

--- go-app.bat
+++ go-app.bat
@@ -2,5 +2,6 @@ set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 cd /d "%SCRIPT_DIR%"
 set "PATH=%SCRIPT_DIR%\venv;%PATH%"
-venv\python.exe -I app.py zh_CN
+set PYTHONPATH=.
+venv\python.exe -s app.py zh_CN
 pause
